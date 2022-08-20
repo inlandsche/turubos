616808d1ac94c1b27307e7ed31f5f161cf09ab12
Qolandar Annuri <79891678+inlandsche@users.noreply.github.com>
2022-08-21 00:31:42 +0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,4 +1,4 @@
-var source = "turulah.mp3";
+var source = "https://github.com/inlandsche/turubos/blob/3b1cbf3c49823a08c05c20d34d3f6c6b12b6c1aa/turulah.mp3";
 var audio = document.createElement('audio');
 
 audio.autoplay = true;
